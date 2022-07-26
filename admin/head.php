3df1aedad98bf615e3855f5be13eef17de9bd7ee
MANI <maniadishu7342@gmail.com>
2022-07-27 00:09:11 +0530

    Update head.php

--- admin/head.php
+++ admin/head.php
@@ -1,8 +1,6 @@
-  <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
-
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,14 +13,13 @@
     <link href="css/bootstrap.min.css" rel="stylesheet">
     
 	
-	<!-- Default Layout -->
-	<link href="css/style.css" rel="stylesheet">
+    <!-- Default Layout -->
+    <link href="css/style.css" rel="stylesheet">
 
     
     <!-- Custom CSS -->
     <link href="css/sb-admin-2.css" rel="stylesheet">
     
-
     
     <link rel="stylesheet" type="text/css" href="css/b.css">
 
