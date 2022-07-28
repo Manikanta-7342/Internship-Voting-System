d881056f316bb1f4027137d11b2c07ad6dbad5e3
likith5 <lavinelev@gmail.com>
2022-07-28 07:57:33 +0530

    category page edited
    added a flute and aligned the logo in mobile mode  and added favicon to it

--- UserVoting/category.php
+++ UserVoting/category.php
@@ -1,22 +1,77 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-	<link rel="icon" type="image/x-icon" href="favicon/TI3.png">
+	<link rel="icon" type="image/x-icon" href="https://competition.youngartiste.com/assets/img/siff.png">
 		<title>SIFF Young Artiste</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="assets/css/main.css" />
+		
+    <title>Category  </title>
+  
+		<style>
+			body{
+				background-color: rgb(241,240,219);
+			}
+ .logo123{
+	width:200px;
+	padding-top:50px;
+	z-index: 1;
+	
+	
+ }
+ .dot123{
+	width:200px;
+	padding-top:50px;
+	z-index: 1;
+	margin-left: 50%;
+	
+ }
+ .flute1234{
+	
+  position:absolute;
+ 
+  width:150px;
+  height:850px;
+  bottom: 20%;
+  margin-top:30%;
+  margin-left:45%;
+ 
+  z-index: -1;
+  -ms-transform: rotate(270deg); /* IE 9 */
+  transform: rotate(270deg);
+
+ }
+ @media only screen and (max-width: 800px) {
+	.flute1234{
+		/* width:100px;
+  height:300px;
+  margin-top:35%;
+  margin-right: 10%;
+  margin-left: 30%; */
+visibility: hidden;
+	}
+	.logo12345{
+		display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+	}
+ }
+</style>
 	</head>
 	<?php include("../VideosPage/sess.php")?>
 	<body class="is-preload">
+		
 		<!-- Wrapper -->
 			<div id="wrapper">
 				<!-- Main -->
 					<div id="main">
 						<div class="inner">
 							<header>
-								 <a href="https://www.youngartiste.com"><img src="image/young-artiste-white.png" style="width : 250px; height:auto; background-color: #e0ab68; position : relative; top: 50px;"></a>
-								<p style="font-size:50px; position: relative; top:120px;">Select the category you want to vote from the list below:</p>
+								 <a  href="https://www.youngartiste.com"><img class="logo12345"src="image/young-artiste-white.png" style="width : 250px; height:auto; background-color: #e0ab68; position : relative; top: 50px;"></a>
+								 <p><img  class="flute1234" src="https://competition.youngartiste.com/assets/img/categories.png" alt="flute"></a></p>
+								 <p style="font-size:50px; position: relative; top:120px;">Select the category you want to vote from the list below:</p>
 							</header>
 							<section class="tiles">
 							<?php
